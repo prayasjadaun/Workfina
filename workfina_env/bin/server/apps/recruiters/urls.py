469e4a631694bdc66e32e6d1aba4b0b00dc1dbf4
Prayas Developer <prayashjadaun6@gmail.com>
2025-12-27 13:21:54 +0530

    Move backend out of virtualenv and ignore env

--- workfina_env/bin/server/apps/recruiters/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import HRRegistrationView, hr_profile
-
-urlpatterns = [
-    path('register/', HRRegistrationView.as_view(), name='hr-register'),
-    path('profile/', hr_profile, name='hr-profile'),
-]
